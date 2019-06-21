2eb4e1310539f0ae138788183175e37e092c92ca
Arturo <arturo.delagarza@definityfirst.com>
2019-06-21 17:31:31 -0500

    ïnit commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,27 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+  {
+    path: 'recipes',
+    children: [
+      {
+        path: '',
+        loadChildren: './recipes/recipes.module#RecipesPageModule',
+      },
+      {
+        path: ':recipeId',
+        loadChildren:
+          './recipes/recipe-detail/recipe-detail.module#RecipeDetailPageModule',
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
