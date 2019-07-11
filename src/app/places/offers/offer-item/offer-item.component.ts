d81b89ef610649222111e93e91d516d320574e30
Arturo <arturo.delagarza@definityfirst.com>
2019-07-11 18:29:17 -0500

    chapter 9 completed

---
+++ src/app/places/offers/offer-item/offer-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Place } from '../../services/place.model';
+
+@Component({
+  selector: 'app-offer-item',
+  templateUrl: './offer-item.component.html',
+  styleUrls: ['./offer-item.component.scss'],
+})
+export class OfferItemComponent implements OnInit {
+  @Input() offer: Place;
+  constructor() {}
+
+  ngOnInit() {}
+
+  getDate() {
+    return new Date();
+  }
+}
